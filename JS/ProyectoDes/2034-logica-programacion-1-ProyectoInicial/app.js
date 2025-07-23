315efe4bed574e75c4a0776c2330a3302a3fd8f2
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-07-23 20:47:05 -0300

    Variable/alert/prompt

---
+++ JS/ProyectoDes/2034-logica-programacion-1-ProyectoInicial/app.js
@@ -0,0 +1,9 @@
+
+//Funcion para mostrar un mensaje de alerta
+alert("Hola, mundo!");
+
+// Variable para almacenar el numero ingresado por el usuario
+let numerousuario = 0;
+
+// Solicitar un numero al usuario
+numerousuario = prompt("Indicame un numero por favor:");
