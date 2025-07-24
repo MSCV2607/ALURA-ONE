2199ff94b4cbe23488c99355b599133ebeebaae0
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-07-23 23:06:09 -0300

    Se agrega la sección Break

---
+++ JS/5-Break/app.js
@@ -0,0 +1,31 @@
+//Variables
+let numeroSecreto = 7;
+let numerousuario = 0;
+let intentos = 1;
+while (numerousuario != numeroSecreto) {
+    // Solicitar un numero al usuario
+    numerousuario = prompt("Indicame un numero por favor:");
+
+    // Mostrar el numero ingresado en la consola
+    console.log("El numero ingresado es: " + numerousuario);
+
+    // Validar si el numero ingresado es igual al numero secreto
+    if (numerousuario == numeroSecreto) {
+
+        //Usamos comillas invertidas para interpolar el valor de la variable
+        alert(`Acertaste el numero secreto:  ${numeroSecreto} lo hicieste en ${intentos} intentos`);
+    } else {
+        if (numerousuario < numeroSecreto) {
+            alert("El numero ingresado es menor al numero secreto");
+        } else {
+            alert(`El numero ingresado es mayor al numero secreto`);
+        }
+        // Incrementar el contador de intentos
+        intentos++;
+        // Verificar si se ha superado el limite de intentos
+        if (intentos > 7) {
+            alert(`Has superado el limite de intentos. El numero secreto era: ${numeroSecreto}`);
+            break;
+        }
+    }
+}
