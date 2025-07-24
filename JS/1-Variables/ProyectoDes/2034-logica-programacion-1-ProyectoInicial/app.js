79061e2cca8cf34166e6caf7bd2a4b8e3c06b15f
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-07-23 22:13:02 -0300

    Condicionales

---
+++ JS/1-Variables/ProyectoDes/2034-logica-programacion-1-ProyectoInicial/app.js
@@ -0,0 +1,12 @@
+// //Funcion para mostrar un mensaje de alerta
+// alert("Hola, mundo!");
+
+// Variable para almacenar numero
+let numerousuario = 0;
+let numeroSecreto = 8;
+
+// Solicitar un numero al usuario
+numerousuario = prompt("Indicame un numero por favor:");
+
+// Mostrar el numero ingresado en la consola
+console.log("El numero ingresado es: " + numerousuario);
