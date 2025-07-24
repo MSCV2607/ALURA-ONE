2d273e00b1ea4f0df60ddb9d8de31de9e9e77dff
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-07-23 22:56:11 -0300

    Count

--- JS/4-Loops y Bucles/app.js
+++ JS/4-Loops y Bucles/app.js
@@ -1,6 +1,7 @@
 //Variables
 let numeroSecreto = 7;
 let numerousuario = 0;
+let intentos = 1;
 while (numerousuario != numeroSecreto) {
     // Solicitar un numero al usuario
     numerousuario = prompt("Indicame un numero por favor:");
@@ -12,12 +13,14 @@ while (numerousuario != numeroSecreto) {
     if (numerousuario == numeroSecreto) {
 
         //Usamos comillas invertidas para interpolar el valor de la variable
-        alert(`Acertaste el numero secreto:  ${numeroSecreto}`);
+        alert(`Acertaste el numero secreto:  ${numeroSecreto} lo hicieste en ${intentos} intentos`);
     } else {
         if (numerousuario < numeroSecreto) {
             alert("El numero ingresado es menor al numero secreto");
         } else {
             alert(`El numero ingresado es mayor al numero secreto`);
         }
+        // Incrementar el contador de intentos
+        intentos++;
     }
 }
