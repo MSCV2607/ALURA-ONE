82fc0e35ac39cfefafb4348ef1d6a8b19ff12830
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-07-24 18:54:54 -0300

    Add user attempt handler to game
    Introduced intentoDeUsuario() function in app.js to handle user attempts and linked it to the 'Intentar' button in index.html using the onclick attribute.

--- JS/1-Logica/1-ProyectoInicial/app.js
+++ JS/1-Logica/1-ProyectoInicial/app.js
@@ -4,3 +4,7 @@ titulo.innerHTML = 'JUEGO DEL NUMERO SECRETO';
 let parrafo = document.querySelector('p');
 parrafo.innerHTML = 'Adivina el número del 1 al 100';
 
+//Funcion para manejar el intento del usuario
+function intentoDeUsuario() {
+    alert('Intento del usuario registrado');
+}
