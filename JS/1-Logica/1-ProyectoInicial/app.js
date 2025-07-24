6ef02aca69536835b958b512e883dcc7aa04ddf8
Mauro Sebastian Camors Vecchietti <maurosebavecc@gmail.com>
2025-07-24 18:32:30 -0300

    aprendiendo querySelector

---
+++ JS/1-Logica/1-ProyectoInicial/app.js
@@ -0,0 +1,6 @@
+let titulo = document.querySelector('h1');
+titulo.innerHTML = 'JUEGO DEL NUMERO SECRETO';
+
+let parrafo = document.querySelector('p');
+parrafo.innerHTML = 'Adivina el número del 1 al 100';
+
